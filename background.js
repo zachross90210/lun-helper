81e562ac9d45f5cd8913940be4742f94db2d7e6b
megido_ <rosstindev@gmail.com>
2020-08-18 18:01:46 +0300

    storage usage optimized

--- background.js
+++ background.js
@@ -6,41 +6,41 @@ function makeCardRed(card) {
     card.classList.add('redBg');
 }
 
-function hide(oid, card) {
-    // by passing an object you can define default values e.g.: []
-    chrome.storage.sync.get({ ids: [] }, (result) => {
-        // the input argument is ALWAYS an object containing the queried keys
-        // so we select the key we need
-        const { ids } = result;
+function updateObject(bid, mode, card) {
+    const item = {};
+    item[bid] = true;
 
-        if (ids.includes(oid)) {
-            ids.splice(ids.indexOf(oid), 1);
+    chrome.storage.sync.set(item, () => {
+        chrome.storage.sync.get('type', (res) => {
             if (card) {
-                makeCardWhite(card);
-            }
-        } else {
-            ids.push({ oid });
-            chrome.storage.sync.get('type', (res) => {
-                if (card) {
-                    if (res.type === 'remove') {
-                        card.closest('div.card-grid-cell').remove();
-                    } else {
-                        makeCardRed(card);
-                    }
+                if (mode) {
+                    makeCardWhite(card);
                 }
-            });
-        }
-
-        // set the new array value to the same key
-        chrome.storage.sync.set({ ids }, () => {
+                if (res.type === 'remove') {
+                    card.closest('div.card-grid-cell').remove();
+                } else {
+                    makeCardRed(card);
+                }
+            }
         });
     });
 }
 
+function hide(oid, card) {
+    const bid = `b-${oid}`;
+    chrome.storage.sync.get([bid], (result) => {
+        if (result[bid]) {
+            updateObject(bid, false, card);
+        } else {
+            updateObject(bid, true, card);
+        }
+    });
+}
+
 function MarkIfHidden(oid, card) {
-    return chrome.storage.sync.get('ids', (result) => {
-        const { ids } = result;
-        if (ids.includes(oid)) {
+    const bid = `b-${oid}`;
+    return chrome.storage.sync.get([bid], (result) => {
+        if (result[bid]) {
             chrome.storage.sync.get('type', (res) => {
                 if (res.type === 'remove') {
                     card.closest('div.card-grid-cell').remove();
@@ -59,22 +59,27 @@ function getObjectID(card) {
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('div.card').forEach((card) => {
+        // Object ID
         const oid = getObjectID(card);
 
         // mark card by red background if is hidden
         MarkIfHidden(oid, card);
 
+        // button hide block
         const hideDiv = document.createElement('div');
         hideDiv.setAttribute('class', 'button -icon favorite');
         hideDiv.setAttribute('style', 'right: 50px;');
+
+        // hide button
         const hideButton = document.createElement('button');
         hideButton.setAttribute('style', 'background-color: red;');
         hideButton.textContent = 'hide';
-
         hideButton.addEventListener('click', () => hide(oid, card));
 
+        // add button to block
         hideDiv.append(hideButton);
 
+        // insert hide block before link
         const a = card.querySelector('a');
         card.insertBefore(hideDiv, a.querySelector('a.card-media'));
     });
