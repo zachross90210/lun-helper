c89cc7e6ec1c73b2aee64ac64e26332ecf70ede5
rostyslavnikitin <rosstindev@gmail.com>
2021-03-08 23:00:27 +0200

    add download support

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+function exportValues() {
+    chrome.storage.sync.get(null, (items) => { // null implies all items
+    // Convert object to a string.
+        const result = JSON.stringify(items);
+
+        // Save as file
+        const url = `data:application/json;base64,${btoa(result)}`;
+        chrome.downloads.download({
+            url,
+            filename: 'filename_of_exported_file.json',
+        });
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     chrome.storage.sync.get(['type'], (result) => {
         document.getElementById('type').value = result;
     });
+
+    const exportButton = document.getElementById('exportButton');
+    exportButton.addEventListener('click', () => exportValues());
 
     document.getElementById('form_submit').addEventListener('click', (event) => {
         event.preventDefault();
