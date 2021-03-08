853e0ffa929c21323334cdf64a543a673113a24f
rostyslavnikitin <rosstindev@gmail.com>
2021-03-08 23:24:02 +0200

    add import functionality

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // eslint-disable-next-line no-unused-vars
+function importValues() {
+    const text = document.getElementById('importData').value;
+    const data = JSON.parse(text);
+
+    Object.entries(data).forEach((entry) => {
+        const item = {};
+        // eslint-disable-next-line prefer-destructuring
+        item[entry[0]] = entry[1];
+        chrome.storage.local.set(item, () => {
+            // eslint-disable-next-line no-console
+            console.log(`added ${entry[0]}`);
+        });
+    });
+
+    chrome.storage.local.get(null, (items) => { // null implies all items
+    // Convert object to a string.
+        const result = JSON.stringify(items);
+
+        // Save as file
+        const url = `data:application/json;base64,${btoa(result)}`;
+        chrome.downloads.download({
+            url,
+            filename: 'filename_of_exported_file.json',
+        });
+    });
+}
+
+// eslint-disable-next-line no-unused-vars
 function exportValues() {
     chrome.storage.local.get(null, (items) => { // null implies all items
     // Convert object to a string.
@@ -18,6 +46,11 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('type').value = result;
     });
 
+    // import button click handle
+    const importButton = document.getElementById('importButton');
+    importButton.addEventListener('click', () => importValues());
+
+    // import button click handle
     const exportButton = document.getElementById('exportButton');
     exportButton.addEventListener('click', () => exportValues());
 
