e510efe525e65cf9df29fe59ef7161d5e669a2fd
ros <rosstindev@gmail.com>
2024-10-11 00:33:52 +0300

    update variable name

--- pack.js
+++ pack.js
@@ -13,17 +13,17 @@ try {
     readFileSync(resolve(__dirname, 'build', 'manifest.json'), 'utf8')
   );
 
-  const outdir = 'release';
+  const outDir = 'release';
   const filename = `${base}-v${version}.zip`;
   const zip = new AdmZip();
   zip.addLocalFolder('build');
-  if (!existsSync(outdir)) {
-    mkdirSync(outdir);
+  if (!existsSync(outDir)) {
+    mkdirSync(outDir);
   }
-  zip.writeZip(`${outdir}/${filename}`);
+  zip.writeZip(`${outDir}/${filename}`);
 
   console.log(
-    `Success! Created a ${filename} file under ${outdir} directory. You can upload this file to web store.`
+    `Success! Created a ${filename} file under ${outDir} directory. You can upload this file to web store.`
   );
 } catch (e) {
   console.log(e);
