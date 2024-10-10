be58a36aff7b0a827eb1ec03f01bb5b4e5e042d9
ros <rosstindev@gmail.com>
2024-10-10 15:08:36 +0300

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
         'html/html-extensions': ['.html'],
         'html/indent': '0', // code should start at the beginning of the line (no initial indentation).
         'html/report-bad-indent': 'error',
+        'no-dupe-args': 'error'
     },
     parserOptions: {
         ecmaVersion: 11,
