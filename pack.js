69fecfd40dca08407d092aa0ff720f900eef57d2
ros <rosstindev@gmail.com>
2024-10-10 18:52:28 +0300

    add pack script

---
+++ pack.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import { readFileSync, existsSync, mkdirSync } from 'fs';
+import { parse, resolve } from 'path';
+import AdmZip from 'adm-zip';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+try {
+  const { base } = parse(__dirname);
+  const { version } = JSON.parse(
+    readFileSync(resolve(__dirname, 'build', 'manifest.json'), 'utf8')
+  );
+
+  const outdir = 'release';
+  const filename = `${base}-v${version}.zip`;
+  const zip = new AdmZip();
+  zip.addLocalFolder('build');
+  if (!existsSync(outdir)) {
+    mkdirSync(outdir);
+  }
+  zip.writeZip(`${outdir}/${filename}`);
+
+  console.log(
+    `Success! Created a ${filename} file under ${outdir} directory. You can upload this file to web store.`
+  );
+} catch (e) {
+  console.log(e);
+  console.error('Error! Failed to generate a zip file.');
+}
