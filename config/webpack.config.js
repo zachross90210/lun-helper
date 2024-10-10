b74e7866d09ba68962cb6e6977c0a0f80443426d
ros <rosstindev@gmail.com>
2024-10-10 18:51:36 +0300

    add webpack config

---
+++ config/webpack.config.js
@@ -0,0 +1,17 @@
+'use strict';
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
+import PATHS from './paths.js';
+
+// Merge webpack configuration files
+const config = (env, argv) =>
+  merge(common, {
+    entry: {
+      popup: PATHS.src + '/popup.js',
+      contentScript: PATHS.src + '/contentScript.js',
+      background: PATHS.src + '/background.js',
+    },
+    devtool: argv.mode === 'production' ? false : 'source-map',
+  });
+
+export default config;
